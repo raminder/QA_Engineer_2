a0bc64b397f8019d525b938630866c31d2f8a9c0
raminder <kaur.raminder811@gmail.com>
2020-06-15 12:49:26 +0530

    Added base project

---
+++ features/support/world/index.js
@@ -0,0 +1,40 @@
+const { setWorldConstructor, setDefaultTimeout } = require('cucumber');
+const { Builder, Capabilities } = require('selenium-webdriver');
+require('chromedriver');
+
+const DEFAULT_TIMEOUT = 60000;
+const BASE_URL = 'https://jobs.economist.com';
+
+function buildDriver() {
+  const chromeCapabilities = Capabilities.chrome();
+  const chromeOptions = {
+    args: ['incognito', 'window-size=1920,1080'],
+  };
+
+  chromeCapabilities.set('chromeOptions', chromeOptions);
+
+  return new Builder()
+    .forBrowser('chrome')
+    .withCapabilities(chromeCapabilities)
+    .build();
+}
+
+class CustomWorld {
+  goToJobsPage() {
+    return this.driver.get(BASE_URL);
+  }
+
+  start() {
+    this.driver = buildDriver();
+  }
+
+  async end() {
+    await this.driver.close();
+    return this.driver.quit();
+  }
+}
+
+setWorldConstructor(CustomWorld);
+setDefaultTimeout(DEFAULT_TIMEOUT);
+
+module.exports = new CustomWorld();
